d2325dd29c652a672da7715be17992c8893cb38e
Сергей Антонов <antonovseantonoff@yandex.ru>
2022-11-26 21:07:23 +0300

    Corrected the name of the variables

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -9,22 +9,22 @@ from django.shortcuts import get_object_or_404
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     passcard_visits = []
-    serialized = Visit.objects.filter(passcard=passcard)
-    for visit in serialized:
+    visits = Visit.objects.filter(passcard=passcard)
+    for visit in visits:
         duration = get_duration(visit)
-        time = format_duration(duration)
-        visit_ = (
+        formatted_duration = format_duration(duration)
+        formatted_entered_at = (
             timezone.localtime(visit.entered_at).strftime(
                 '%d %B %Y г. %H:%M:%S'
             )
         )
         visit_long = is_visit_long(visit)
-        visited = {
-            'entered_at': visit_,
-            'duration': time,
+        serialized_visit = {
+            'entered_at': formatted_entered_at,
+            'duration': formatted_duration,
             'is_strange': visit_long
         }
-        passcard_visits .append(visited)
+        passcard_visits .append(serialized_visit)
     context = {
         'passcard': passcard,
         'this_passcard_visits': passcard_visits
